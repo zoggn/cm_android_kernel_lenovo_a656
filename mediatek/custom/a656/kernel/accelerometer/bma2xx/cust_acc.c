27c461ba6080cdb09701bfff098791c5b2ae85a8
zoggn <kurkov.nikita@gmail.com>
2017-11-09 20:06:17 +0200

    a656:bma2x: update cust and enable driver

--- mediatek/custom/a656/kernel/accelerometer/bma2xx/cust_acc.c
+++ mediatek/custom/a656/kernel/accelerometer/bma2xx/cust_acc.c
@@ -5,8 +5,8 @@
 
 /*---------------------------------------------------------------------------*/
 static struct acc_hw cust_acc_hw = {
-    .i2c_num = 2,
-    .direction = 6,
+    .i2c_num = 3,
+    .direction =2, //tried 1,3
     .power_id = MT65XX_POWER_NONE,  /*!< LDO is not used */
     .power_vol= VOL_DEFAULT,        /*!< LDO is not used */
     .firlen = 0, //old value 16                /*!< don't enable low pass fileter */
@@ -16,5 +16,3 @@ struct acc_hw* get_cust_acc_hw(void)
 {
     return &cust_acc_hw;
 }
-
-
